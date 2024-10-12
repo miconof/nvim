a49254fdf6bec2d923b82430e3f90ff7cc087cfe
Adrià Armejach <adria.armejach@bsc.es>
2024-10-12 02:28:00 +0200

    Add vim-tmux-navigator, and nvim resize with M+arrow

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Delete default resize window keymaps
+vim.keymap.del("n", "<C-Up>")
+vim.keymap.del("n", "<C-Down>")
+vim.keymap.del("n", "<C-Left>")
+vim.keymap.del("n", "<C-Right>")
+
+-- Move to window/pane using the <ctrl> + arrow keys
+vim.keymap.set("n", "<C-Left>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("n", "<C-Down>", "<cmd>TmuxNavigateDown<cr>", { desc = "Go to Lower Window", remap = true })
+vim.keymap.set("n", "<C-Up>", "<cmd>TmuxNavigateUp<cr>", { desc = "Go to Upper Window", remap = true })
+vim.keymap.set("n", "<C-Right>", "<cmd>TmuxNavigateRight<cr>", { desc = "Go to Right Window", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<M-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<M-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<M-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<M-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,14 @@
+-- make nvim aware of tmux navigation
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {},
+  },
+}
