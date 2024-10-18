9a049ba71857e6a56d33992b7fc78bca7c8d1d2e
Adrià Armejach <adria.armejach@bsc.es>
2024-10-18 11:37:15 +0200

    Add old vim defaults

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.o.cursorline = false
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.breakindent = true
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.relativenumber = false
